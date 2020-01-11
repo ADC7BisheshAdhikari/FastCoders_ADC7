74e88fb32be23816a27c1bbc238d3c126e1bab25
SameerBasnet <np03a180095@heraldcollege.edu.np>
2020-01-11 17:38:50 +0545

    testing

--- FastCoders/easyjob/views.py
+++ FastCoders/easyjob/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template,Context
+
 
 # Create your views here.
+def Company(request):
+    return render(request,'company.html')
+def Applicant(request):
+    return render(request,'registerform.html')
+def Firstpage(request):
+    return render(request,'index.html')
