f2489f7343301e8132e9035a7386f479eb5fd62a
ravisodari27 <np03a180236@heraldcollege.edu.np>
2020-01-11 21:59:02 +0545

    done search functionality

--- FastCoders/easyjob/views.py
+++ FastCoders/easyjob/views.py
@@ -10,3 +10,24 @@ def Applicant(request):
     return render(request,'registerform.html')
 def Firstpage(request):
     return render(request,'index.html')
+
+
+
+
+
+def search1(request):
+    if request.method=='POST'
+        srch=request.POST['srh']
+
+
+        if srch:
+            match = Post.objects.filter(Q(name__icontains=srch))
+        
+
+            if match:
+                return render(request,'company.html',{'sr':match}
+            else:
+                message.error(request,'no result found')
+        else:
+            return HttpResponseRedirect(/search1/)
+    return render(request,'company.html')
