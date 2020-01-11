2e88c443dbc9e94379c47f068626b4c5024144bf
ADC7BisheshAdhikari <np03a180063@heraldcollege.edu.np>
2020-01-11 19:56:32 +0545

    Some changes in upload

--- FastCoders/easyjob/urls.py
+++ FastCoders/easyjob/urls.py
@@ -1,23 +1,11 @@
 
 from django.urls import path
 from .views import *
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-
-
 
 urlpatterns =[
-    path('company/',Company),
-    path('applicant/',Applicant),
-    path('firstpage/',Firstpage ),
-    path('profilelist/', views.profile_list),
-    path('profilelist/upload', views.upload_profile),
-
-
+    path('company/',company),
+    path('applicant/',applicant),
+    path('firstpage/',firstPage),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- FastCoders/easyjob/views.py
+++ FastCoders/easyjob/views.py
@@ -2,31 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template,Context
 from django.core.files.storage import FileSystemStorage
-from .models import Profile
+
 
 
 
 # Create your views here.
 
-def Company(request):
+def company(request):
     return render(request,'company.html')
 
-def Applicant(request):
+def applicant(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
         fs= FileSystemStorage()
         fs.save(uploaded_file.name,uploaded_file)
-    return render(request,'profile.html')
-    
-def profile_list(request):
-    profiles= Profile.object.all()
-    return render(request,'profile.html',{'profiles' : profile_list})
-   
+    return render(request,'registerform.html')
 
-def upload_profile(request):
-     return render(request,'profile.html')
-
-
-def Firstpage(request):
+def firstPage(request):
     return render(request,'index.html')
-
