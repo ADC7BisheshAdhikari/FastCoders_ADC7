6c49c2e230300f8c77cdee6f8c44e6aaa4fdba92
ADC7BisheshAdhikari <np03a180063@heraldcollege.edu.np>
2020-01-11 17:31:03 +0545

    worked on upload and download

--- FastCoders/easyjob/models.py
+++ FastCoders/easyjob/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email_id = models.CharField(max_length=20)
+    phoneno = models.IntegerField(10)
+    education = models.CharField(max_length=100)
+    experience = models.CharField(max_length=20)
+    document = models.FileField(upload_to='profiles/pdfs/')
+
+    def __str__(self):
+        return self.first_name
+
+
+
+
+
+
--- FastCoders/easyjob/urls.py
+++ FastCoders/easyjob/urls.py
@@ -1,11 +1,23 @@
 
 from django.urls import path
 from .views import *
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 
 urlpatterns =[
     path('company/',Company),
     path('applicant/',Applicant),
     path('firstpage/',Firstpage ),
+    path('profilelist/', views.profile_list),
+    path('profilelist/upload', views.upload_profile),
+
+
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
